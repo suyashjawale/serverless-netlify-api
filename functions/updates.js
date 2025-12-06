7ef312010909aafcd26f5c5b33a3d98b43b1f8d5
Suyash Jawale <suyashjawale245@gmail.com>
2025-12-06 16:45:13 +0530

    testing

--- functions/updates.js
+++ functions/updates.js
@@ -31,42 +31,36 @@ exports.handler = async (event, context) => {
             event.headers["x-real-ip"] ||
             firestore.collection('visitors').doc().id;
         clientIp = clientIp.split(",")[0]
-        // await firestore.collection('visitors').doc(clientIp).set({
-        //     'IP': clientIp,
-        //     'headers': {
-        //         'user-agent': event.headers["user-agent"],
-        //         'sec-ch-ua-mobile': event.headers["sec-ch-ua-mobile"],
-        //         'sec-ch-ua-model': event.headers["sec-ch-ua-model"],
-        //         'sec-ch-ua-platform': event.headers["sec-ch-ua-platform"],
-        //         'sec-ch-ua-platform-version': event.headers["sec-ch-ua-platform-version"]
-        //     }, visits: FieldValue.arrayUnion({
-        //         'visit-time': new Date().toISOString()
-        //     })
-        // }, { merge: true });
 
-        // const message = {
-        //     chat_id: process.env.CHAT_ID,
-        //     text: `Visitor - ${clientIp}`,
-        //     reply_markup: {
-        //         inline_keyboard: [
-        //             [
-        //                 { text: "Get Details", callback_data: clientIp }
-        //             ]
-        //         ]
-        //     }
-        // };
+        await firestore.collection('visitors').doc(clientIp).set({
+            'IP': clientIp,
+            'headers': event.headers, visits: FieldValue.arrayUnion({
+                'visit-time': new Date().toISOString()
+            })
+        }, { merge: true });
 
-        // await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-        //     method: "POST",
-        //     headers: { "Content-Type": "application/json" },
-        //     body: JSON.stringify(message)
-        // });
-        const parser = UAParser(event.headers).withClientHints();
-        await firestore.collection('visitors').doc(clientIp).set(event.headers);
+        const message = {
+            chat_id: process.env.CHAT_ID,
+            text: `Visitor - ${clientIp}`,
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "Get Details", callback_data: clientIp }
+                    ]
+                ]
+            }
+        };
+
+        await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(message)
+        });
+
         return {
             statusCode: 200,
             headers,
-            body: JSON.stringify(parser, null, 2)
+            body: JSON.stringify({ok:"ok"})
         };
 
     } catch (error) {
