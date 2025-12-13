c0afc8b10221126a70ca52a70a0649143d2ae6b9
Suyash Jawale <suyashjawale245@gmail.com>
2025-12-13 10:05:25 +0530

    get creds

---
+++ functions/getCredentials.js
@@ -0,0 +1,75 @@
+const admin = require('firebase-admin');
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+};
+
+// Initialize Firebase only once
+if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+}
+
+const firestore = admin.firestore();
+
+const SECRET_PASSWORD = process.env.AUTHENTICATION_CODE;
+
+exports.handler = async (event) => {
+    // Handle preflight request
+    if (event.httpMethod === 'OPTIONS') {
+        return { statusCode: 200, headers };
+    }
+
+    // Only allow POST
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            headers,
+            body: JSON.stringify({ error: 'Method not allowed' }),
+        };
+    }
+
+    try {
+        const body = JSON.parse(event.body || '{}');
+        const { password } = body;
+
+        // Validate input
+
+        if (password.length > 20) {
+            return {
+                statusCode: 400,
+                headers,
+                body: JSON.stringify({ error: 'Invalid' }),
+            };
+        }
+
+        if (password.trim().length != 0 && password.toString() !== SECRET_PASSWORD) {
+            return {
+                statusCode: 401,
+                headers,
+                body: JSON.stringify({ error: 'Invalid password' }),
+            };
+        }
+
+        const users = await firestore.collection('instaBot').doc('users').get();
+        const creds = await firestore.collection('credentials').doc('instaStoryBot').get();
+
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ "users": users.data()['records'], "creds": creds.data() }),
+        };
+
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
