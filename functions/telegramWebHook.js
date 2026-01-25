02801fd78c2004b2423159b6fb472900b0c8f267
Suyash Jawale <suyashjawale245@gmail.com>
2026-01-25 17:20:39 +0530

    IP Issue Solved

--- functions/telegramWebHook.js
+++ functions/telegramWebHook.js
@@ -61,7 +61,7 @@ exports.handler = async (event, context) => {
         message += `\n\n<b>Browser</b>\n${parser.browser.name} <code>${parser.browser.version}</code> <code>${userAgent.data().headers['sec-ch-ua-full-version-list']}</code>\n\n__________________\n\n`;
 
         message += `<b>Location</b>\n<a href="https://www.google.com/maps/search/?api=1&query=${xml.lat},${xml.lon}">${xml.city}, ${xml.regionName}, ${xml.country}, ${xml.continent}</a>`;
-        message += `\n\n<b>ISP</b>\n${xml.isp} - ${xml.isp}\n${xml.org}`;
+        message += `\n\n<b>ISP</b>\n${xml.isp}\n${xml.org}`;
 
         await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
             method: "POST",
